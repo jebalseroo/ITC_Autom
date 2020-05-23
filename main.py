16ba2b8149c27b8f6feab5b8a7a14278d7662529
jebalseroo <38665510+jebalseroo@users.noreply.github.com>
2020-05-23 08:10:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,46 @@ a = AFD.AFD("fileAFD.txt")
 
 # b = ProcesamientoCadenaAFD.ProcesamientoCadenaAFD()
 
-
+def lectura(tipo):
+    print("|------------------------------------------|")
+    print("|                Automatas                 |")
+    print("|------------------------------------------|")
+    print("| 1) Escribir Automata por consola         |")
+    print("| 2) Cargar Automata por archivo           |")
+    print("|------------------------------------------|")
+    Numopc1 = int(input())
+    if Numopc1 == 1:
+        print("Ecribiendo "+ tipo)
+        Escribir(tipo) ##llamar la funcion para empezar a escribir el automata
+    if Numopc1 == 2:
+        print("cargando "+ tipo)
+        Cargar(tipo) #llamar la funcion para empezar a cargar el automata
+ini = 1
+while ini == 1:
+    print("|------------------------------------------|")
+    print("|                Automatas                 |")
+    print("|------------------------------------------|")
+    print("|  Con que tipo automata quiere trabajar   |")
+    print("| 1) AFD                                   |")
+    print("| 2) AFN                                   |")
+    print("| 3) AFN-landa                             |")
+    print("| 4) Salir                                 |")
+    print("|------------------------------------------|")
+    Numopc = int(input())
+    if Numopc == 1:
+        print("AFD")
+        lectura("AFD")
+    if Numopc == 2:
+        print("AFN")
+        lectura("AFN")
+    if Numopc == 3:
+        print("AFN-Landa")
+        lectura("AFN-Landa")
+    if Numopc == 4:
+        print("salir")
+        ini = 2
+    else:
+        print("escoge una opccion valida")
 
 
 
